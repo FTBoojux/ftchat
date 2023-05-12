ba7418e31bd5fc7d4edc4210a91da0dd1b8605e5
Boojux <iamboojux@gmail.com>
2023-05-12 17:07:47 +0800

    page base build

--- src/app/page.js
+++ src/app/page.js
@@ -1,96 +1,92 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import React, { useState } from 'react';
+import styles from '../styles/home.module.css';
+import {
+    Box,
+    Button,
+    ButtonGroup,
+    Card,
+    CardActionArea,
+    CardActions,
+    CardHeader,
+    FormControl, Input, InputLabel,
+    TextField
+} from "@mui/material";
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-	发丝欸，萨尼铁塔
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+const HomePage = () => {
+    const [isLogin, setIsLogin] = useState(true);
+    const [email,setEmail] = useState("")
+    const [password,setPassword] = useState("")
+    const login = ()=>{
+        console.log("email",email)
+        console.log("password",password)
+    }
+    return (
+        <div className={styles.container} >
+            <div className={styles.box}>
+                <Box>
+                    <Card variant="outlined">
+                        {
+                            isLogin ?
+                                <CardActionArea>
+                                    <div>
+                                        <FormControl
+                                            variant={"standard"}
+                                        >
+                                            <InputLabel
+                                                htmlFor={"standard-adornment-password"}
+                                            >
+                                                邮箱
+                                            </InputLabel>
+                                            <Input
+                                                id="standard-adornment-email"
+                                                onChange={(event)=>setEmail(event.target.value)}
+                                            >
+
+                                            </Input>
+                                        </FormControl>
+                                    </div>
+                                    <div>
+
+                                        <FormControl
+                                            variant={"standard"}
+                                        >
+                                            <InputLabel
+                                                htmlFor={"standard-adornment-password"}
+                                            >
+                                                密码
+                                            </InputLabel>
+                                            <Input
+                                                id="standard-adornment-password"
+                                                type="password"
+                                                onChange={(event)=>setPassword(event.target.value)}
+                                            >
+
+                                            </Input>
+                                        </FormControl>
+                                    </div>
+                                    <div className={"align"}>
+                                        <FormControl>
+                                            <Button onClick={()=>login()}>确认</Button>
+                                        </FormControl>
+                                    </div>
+                                </CardActionArea>
+                                :
+                                <CardActionArea>
+                                    注册
+                                </CardActionArea>
+                        }
+                        <div className={"align"}>
+                            <ButtonGroup variant="contained" aria-label="outlined primary button group">
+                                <Button onClick={()=>setIsLogin(true) } >登录</Button>
+                                <Button onClick={()=>setIsLogin(false) }>注册</Button>
+                            </ButtonGroup>
+                        </div>
+                    </Card>
+                </Box>
+            </div>
         </div>
-      </div>
+    );
+};
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default HomePage;

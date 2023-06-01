178b555799eb821eb29bf5877c35abcab376c124
Boojux <iamboojux@gmail.com>
2023-06-01 15:02:36 +0800

    rewrite login.js,add MyFetch

---
+++ src/app/api/MyFetch.js
@@ -0,0 +1,16 @@
+import { tauri } from "@tauri-apps/api";
+export default async function MyFetch(url, options = {}) {
+    const defaultOptions = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    const token = await tauri.app.invoke('plugin:storage|read', { key: 'access_token' });
+
+    if (token) {
+        defaultOptions.headers['Authorization'] = `Bearer ${token}`;
+    }
+
+    return fetch(url, { ...defaultOptions, ...options });
+}

178a76f9f40a4d7de40b0a8ad38792f18d3171e6
Boojux <iamboojux@gmail.com>
2023-06-05 14:02:22 +0800

    NavLink style change

--- src/app/main/page.js
+++ src/app/main/page.js
@@ -5,16 +5,30 @@ import Drawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
-
+import { BrowserRouter as Router, Route, Link, Routes, NavLink, useLocation} from 'react-router-dom';
+import ChatIcon from '@mui/icons-material/Chat';
+import PersonIcon from '@mui/icons-material/Person';
+import SettingsIcon from '@mui/icons-material/Settings';
 // Here are your pages
 const Messages = () => <div>Messages</div>;
 const Contacts = () => <div>Contacts</div>;
 const Settings = () => <div>Settings</div>;
 
-const drawerWidth = 240;
+const drawerWidth = 40;
 
 const Main = () => {
+    // 定义一个可以接受路径并变化颜色的图标组件
+  const ColoredIcon = ({ to, children }) => {
+    const location = useLocation();
+
+    // 根据当前路径是否匹配，来选择图标颜色
+    const color = location.pathname === to ? 'blue' : 'inherit';
+
+    // 使用 color 作为图标的颜色
+    const iconWithColor = React.cloneElement(children, { style: { color } });
+
+    return iconWithColor;
+  };
   return (
     <Router>
       <Box sx={{ display: 'flex' }}>
@@ -23,25 +37,33 @@ const Main = () => {
           sx={{
             width: drawerWidth,
             flexShrink: 0,
-            '& .MuiDrawer-paper': {
-              width: drawerWidth,
-              boxSizing: 'border-box',
-            },
+            // '& .MuiDrawer-paper': {
+            //   width: drawerWidth
+            //   // boxSizing: 'border-box',
+            // },
           }}
         >
           <List>
-            <ListItem button component={Link} to="/messages">
-              <ListItemText primary="消息" />
+            <ListItem button component={NavLink} to="/messages">
+              <ColoredIcon to="/messages">
+                <ChatIcon />
+              </ColoredIcon>
             </ListItem>
-            <ListItem button component={Link} to="/contacts">
-              <ListItemText primary="联系人" />
+            <ListItem button component={NavLink} to="/contacts">
+              <ColoredIcon to="/contacts">
+                <PersonIcon />
+              </ColoredIcon>
             </ListItem>
-            <ListItem button component={Link} to="/settings">
-              <ListItemText primary="设置" />
+            <ListItem button component={NavLink} to="/settings">
+              <ColoredIcon to="/settings">
+                <SettingsIcon />
+              </ColoredIcon>
             </ListItem>
           </List>
         </Drawer>
-        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Box component="main" 
+          sx={{ flexGrow: 1, p: 3 }}
+          >
           <Routes>
             <Route path="/messages" element={<Messages />} />
             <Route path="/contacts" element={<Contacts />} />

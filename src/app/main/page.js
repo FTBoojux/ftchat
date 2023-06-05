d43651e6ef9869dcb0d96c7d360b228b1b9239dd
Boojux <iamboojux@gmail.com>
2023-06-05 14:58:34 +0800

    style change

--- src/app/main/page.js
+++ src/app/main/page.js
@@ -35,11 +35,11 @@ const Main = () => {
           variant="permanent"
           sx={{
             width: drawerWidth,
-            flexShrink: 0,
-            '& .MuiDrawer-paper': {
-              width: drawerWidth,
-              boxSizing: 'border-box',
-            },
+            flexShrink: 0
+            // '& .MuiDrawer-paper': {
+            //   width: drawerWidth,
+            //   boxSizing: 'border-box',
+            // },
           }}
         >
           <List>

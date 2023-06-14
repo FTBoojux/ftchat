83b3c75fed8eefdec3d6453c0b671a3263bfeb82
Boojux <iamboojux@gmail.com>
2023-06-14 16:11:37 +0800

    modify logout

--- src/app/main/settings/page.js
+++ src/app/main/settings/page.js
@@ -1,15 +1,25 @@
+import MyFetch from '@/app/api/MyFetch';
 import { Button } from '@mui/material';
 import React from 'react';
 
 const Settings = () => {
 
   const handleLogout = () => {
+    MyFetch(`/api/account/logout/`, {
+      method: 'POST',
+    }).then(response=>response.json())
+    .then((data) => {
+      console.log(data);
+    }).catch((error) => {
+      console.error(error);
+    })
     localStorage.clear();
     window.location.reload();
   }
 
   return (
     <div>
+      
       <Button variant="contained" onClick={handleLogout}>退出</Button>
     </div>
   );
